3c6114584e017a7496da81d0d2e89f97517fb45f
coderyeasin <yeasin3233@gmail.com>
2022-02-06 03:11:41 +0600

    title add

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+
 import FooterPage from "../components/footerPage/FooterPage";
 
 export default function Home ()
 {
   return (
     <div>
+      <h3 style={{textAlign:'center'}}>CorporateZone!</h3>
       <h1>hello next js</h1>
       <FooterPage />
     </div>
