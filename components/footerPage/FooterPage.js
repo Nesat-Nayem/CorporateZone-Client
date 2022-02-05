1dabb175634037157879c2e7010faf123c720222
Morsalin <morsalinkausar043@gmail.com>
2022-02-05 20:23:42 +0600

    done

---
+++ components/footerPage/FooterPage.js
@@ -0,0 +1,19 @@
+
+const footerPage = () => {
+  return (
+    <>
+          <h2 className="title">hello world this is footer page!</h2>
+          <style jsx>
+              {
+                  `
+                  .title{
+                      color:red;
+                  }
+                  `
+            }
+          </style>
+    </>
+  )
+}
+
+export default footerPage
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import FooterPage from "../components/footerPage/FooterPage";
+
 export default function Home() {
   return (
     <div>
       <h1>hello next</h1>
+      <FooterPage/>
     </div>
   );
 }
