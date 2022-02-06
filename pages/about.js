c335f2d382603f5679e2db4cdbce19c0f834ab83
Nesat-Nayem <nesatnayem042@gmail.com>
2022-02-06 07:02:21 +0600

    clean

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+            <h1>this is about page</h1>
+        </div>
+    );
+};
+
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>webdev news</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      
       <h1>hello next</h1>
+      <a href="/about">about</a>
     </div>
   );
 }
