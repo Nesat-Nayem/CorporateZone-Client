db966701b65654388f4303d0e7bc5c3a7ca7d7e0
Morsalin <morsalinkausar043@gmail.com>
2022-02-07 13:22:20 +0600

    done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Footer from "../components/Home/Footer/Footer";
+import Pricing from "../components/Home/pricing/Pricing";
 
 
 export default function Home() {
   return (
     <>
+      <Pricing/>
       <Footer/>
     </>
   );
