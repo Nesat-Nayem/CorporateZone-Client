5fb8fb592cd34cdd0ae8d3bd2d299bbeb3d8cf78
coderyeasin <yeasin3233@gmail.com>
2022-02-07 13:08:12 +0600

    openings_from_company

--- components/Home/CompanyJobs/CompanyJobs.js
+++ components/Home/CompanyJobs/CompanyJobs.js
@@ -1,8 +1,85 @@
 
 const CompanyJobs = () => {
     return (
-        <div>
-            <h3>Company Jobs</h3>
+        <div className="my-5">
+            <div className="md:flex justify-between items-center md:w-8/12 mx-auto company_heading relative my-20">
+                <h3 className="text-3xl after:content-[''] after:ml-0.5 after:text-red-500 after:absolute after:border-t-2 after:w-20 after: after:border-cyan-500 after:top-11 after:left-0 after:bottom-0 after:right-0 font-medium text-slate-700">Top Hiring Companies</h3>
+                <ul className="md:flex items-center">
+                    <li className="mx-3">prev</li>
+                    <li className="mx-3">next</li>
+                </ul>
+            </div>
+   
+            <div className="md:flex justify-center items-center my-20 company_card">
+                
+                <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 w-1/4 mx-2 ring-slate-900/5 text-center shadow-xl">
+                <div>
+                    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                    <svg className="h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+                    </span>
+                </div>
+                <h3 className="text-slate-900 dark:text-white mt-3 text-base font-medium tracking-tight">AmaZon</h3>
+                <span className="text-slate-400 dark:text-white text-sm font-medium tracking-tight">(SouthAfrica)</span> <br />
+                <a href="#" className="text-slate-200 dark:text-slate-400 block md:w-1/3 mx-auto mt-3 text-sm bg-slate-800 p-2 rounded-lg">
+                    7 Openings 
+                </a>
+                </div>
+
+                <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 w-1/4 mx-2 ring-slate-900/5 text-center shadow-xl">
+                <div>
+                    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                    <svg className="h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+                    </span>
+                </div>
+                <h3 className="text-slate-900 dark:text-white mt-3 text-base font-medium tracking-tight">AmaZon</h3>
+                <span className="text-slate-400 dark:text-white text-sm font-medium tracking-tight">(SouthAfrica)</span> <br />
+                <a href="#" className="text-slate-200 dark:text-slate-400 block md:w-1/3 mx-auto mt-3 text-sm bg-slate-800 p-2 rounded-lg">
+                    7 Openings 
+                </a>
+                </div>
+        
+                <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 w-1/4 mx-2 ring-slate-900/5 text-center shadow-xl">
+                <div>
+                    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                    <svg className="h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+                    </span>
+                </div>
+                <h3 className="text-slate-900 dark:text-white mt-3 text-base font-medium tracking-tight">AmaZon</h3>
+                <span className="text-slate-400 dark:text-white text-sm font-medium tracking-tight">(SouthAfrica)</span> <br />
+                <a href="#" className="text-slate-200 dark:text-slate-400 block md:w-1/3 mx-auto mt-3 text-sm bg-slate-800 p-2 rounded-lg">
+                    7 Openings 
+                </a>
+                </div>
+                
+                <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 w-1/4 mx-2 ring-slate-900/5 text-center shadow-xl">
+                <div>
+                    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                    <svg className="h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+                    </span>
+                </div>
+                <h3 className="text-slate-900 dark:text-white mt-3 text-base font-medium tracking-tight">AmaZon</h3>
+                <span className="text-slate-400 dark:text-white text-sm font-medium tracking-tight">(SouthAfrica)</span> <br />
+                <a href="#" className="text-slate-200 dark:text-slate-400 block md:w-1/3 mx-auto mt-3 text-sm bg-slate-800 p-2 rounded-lg">
+                    7 Openings 
+                </a>
+                </div>
+                
+                <div className="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 w-1/4 mx-2 ring-slate-900/5 text-center shadow-xl">
+                <div>
+                    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+                    <svg className="h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
+                    </span>
+                </div>
+                <h3 className="text-slate-900 dark:text-white mt-3 text-base font-medium tracking-tight">AmaZon</h3>
+                <span className="text-slate-400 dark:text-white text-sm font-medium tracking-tight">(SouthAfrica)</span> <br />
+                <a href="#" className="text-slate-200 dark:text-slate-400 block md:w-1/3 mx-auto mt-3 text-sm bg-slate-800 p-2 rounded-lg">
+                    7 Openings 
+                </a>
+                </div>
+        
+            
+            </div>
+            
         </div>
     );
 };
