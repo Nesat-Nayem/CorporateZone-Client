bcb70c9dd24bb8b7a04b1a8a0ed49698f175c95f
coderyeasin <yeasin3233@gmail.com>
2022-02-09 00:19:54 +0600

    done responsive

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,17 @@
 import Head from "next/head";
-import CategoryJobs from "../components/Home/CategoryJobs/CategoryJobs";
 import CompanyJobs from "../components/Home/CompanyJobs/CompanyJobs";
 import Footer from "../components/Home/Footer/Footer";
 import Header from "../components/Home/Header/Header";
 import HeroSection from "../components/Home/HeroSection/HeroSection";
-import RecentJobs from "../components/Home/RecentJobs/RecentJobs";
 
 
 export default function Home ()
 {
   return (
     <div>
-      <Head>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
-
-      </Head>
-
       <Header />
       <HeroSection />
-      <CategoryJobs />
       <CompanyJobs />
-      <RecentJobs />
       <Footer />
     </div>
   );
