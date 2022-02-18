39f704ec437c200962ff6eed8c69e8bae25e9157
Mohammed Rakib <mohammedrakib81336@gmail.com>
2022-02-18 18:36:22 +0600

    jobs fetched and show in UI with filtering options

--- components/Jobs/PostedJobs.js
+++ components/Jobs/PostedJobs.js
@@ -1,118 +1,105 @@
 import React from "react";
 import Image from "next/image";
-import { MdMoreTime, MdPedalBike } from "react-icons/md";
+import {
+  MdMoreTime,
+  MdPedalBike,
+  MdMobileFriendly,
+  MdSportsTennis,
+  MdAddTask,
+} from "react-icons/md";
+import { FaMoneyBillAlt } from "react-icons/fa";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useEffect } from "react";
+import PostedJob from "./PostedJob";
+import { useRouter } from "next/router";
 
-const PostedJobs = () => {
-  // Filtered Options
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+const PostedJobs = (props) => {
+  const [jobs, setJobs] = useState(props.jobs);
+  const [category, setCategory] = useState();
+
+  const router = useRouter();
+
+  const filterJobs = async (e) => {
+    setCategory(e.target.value);
+    const response = await fetch(
+      `https://sheltered-journey-99057.herokuapp.com/jobs?jobType=${e.target.value}`
+    );
+    const data = await response.json();
+    setJobs(data.data);
+    router.push(`/jobs?jobType=${e.target.value}`, undefined, {
+      shallow: true,
+    });
+  };
 
   return (
-    <div>
-      {/* Search Start */}
-      <div className="md:flex justify-center items-center md:max-w-[75%] md:mx-auto relative mt-12">
-        <div className="my-12 md:w-5/12 focus:border-cyan-600 search_jobs">
-          <label className="relative block">
-            <span className="sr-only">Search</span>
-            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-              <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
-            </span>
-            <input
-              className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Search Job Keyword..."
-              type="text"
-              name="search"
-            />
-          </label>
+    <section className="bg-gray-100 pb-10">
+      <div className="md:w-9/12 w-11/12 mx-auto">
+        {/* Search Start */}
+        <div className="md:flex justify-center items-center md:max-w-[75%] md:mx-auto relative mt-12">
+          <div className="my-12 md:w-5/12 focus:border-cyan-600 search_jobs">
+            <label className="relative block">
+              <span className="sr-only">Search</span>
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg
+                  className="h-5 w-5 fill-slate-300"
+                  viewBox="0 0 20 20"
+                ></svg>
+              </span>
+              <input
+                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                placeholder="Search Job Keyword..."
+                type="text"
+                name="search"
+              />
+            </label>
+          </div>
+          <button className="bg-cyan-500 p-2 rounded-lg text-slate-200 font-bold w-2/12 md:absolute right-80 border-1 ring ring-inset ring-cyan-600 border-slate-50">
+            Search
+          </button>
         </div>
-        <button className="bg-cyan-500 p-2 rounded-lg text-slate-200 font-bold w-2/12 md:absolute right-80 border-1 ring ring-inset ring-cyan-600 border-slate-50">
-          Search
-        </button>
-      </div>
-      {/* End Search */}
+        {/* End Search */}
 
-      {/* Start Filter */}
-      <div className="md:flex justify-center items-center md:max-w-[75%] md:mx-auto relative mb-12 filter_jobs">
-        <form onSubmit={handleSubmit(onSubmit)} className="jobs_tabs">
-          <select
-            className="bg-cyan-100 text-lg text-center bottom-0 p-2 rounded-lg text-cyan-600 mx-3 job_type"
-            {...register("Type")}
-          >
-            <option value="type">Type</option>
-            <option value="part_time">Part Time</option>
-            <option value="fulltime">Full Time</option>
-            <option value="internship">Internship</option>
-          </select>
-
-          <select
-            className="bg-cyan-100 text-lg text-center bottom-0 p-2 rounded-lg text-cyan-600 mx-3 mx-3"
-            {...register("location")}
-          >
-            <option value="locations">Location</option>
-            <option value="part_time">USA</option>
-            <option value="fulltime">Europe</option>
-            <option value="internship">Middle West</option>
-          </select>
-
-          <select
-            className="bg-cyan-100 text-lg text-center bottom-0 p-2 rounded-lg text-cyan-600 mx-3 mx-3"
-            {...register("experience")}
-          >
-            <option value="experience">Experience</option>
-            <option value="entry_level">Entry Level</option>
-            <option value="mid_level">Mid Level</option>
-            <option value="sr_level">Sr Level</option>
-          </select>
-
-          <input
-            className="bg-cyan-500 p-2 text-white rounded-lg"
-            type="submit"
-          />
-        </form>
-      </div>
-
-      {/* End Filter */}
-
-      {/* Start Jobs Card */}
-      <div className="flex justify-evenly items-center mx-auto my-12 border-2 border-slate-200 w-7/12 rounded-lg jobs_box">
-        <Image
-          className=""
-          width={120}
-          height={120}
-          src="https://github.com/Nesat-Nayem/CorporateZone-Clitent/blob/listed_job/ya/images/first_company.jpg?raw=true"
-        />
-
-        <div className=" w-6/12 job_card">
-          <h3>
-            Front End Engineer <span className="text-slate-500">/ MERN</span>
-          </h3>
-          <article>
-            <p>Jackson Heights, NY</p>
-            <h4>Onsite</h4>
-          </article>
-        </div>
-
-        <div className=" w-4/12 open_job">
-          <div className="flex justify-evenly items-center advantages_icon">
-            <div>
-              Facilities with icon
-              <MdMoreTime />
-              <MdPedalBike />
-            </div>
-            <h4>Salary Icon</h4>
-            <div className="text-center">
-              <h5>Deadline: Feb 20</h5>
-              <button className="bg-cyan-500 text-slate-900 px-3 py-1 rounded-lg">
-                Apply Now
-              </button>
-            </div>
+        {/* Start Filter */}
+        <div className="md:flex justify-between items-center md:max-w-[75%] md:mx-auto relative mb-12 filter_jobs">
+          <div>
+            <h1 className="text-xl font-serif font-bold">Filter Jobs</h1>
+          </div>
+          <div>
+            <select
+              name=""
+              id=""
+              value={category}
+              className="bg-cyan-600 text-white  px-2 py-2  focus:outline-none rounded"
+              onChange={filterJobs}
+            >
+              <option value="all">All Jobs</option>
+              <option value="intern">Intern</option>
+              <option value="full-time">Full-Time</option>
+              <option value="part-time">Part-Time</option>
+            </select>
           </div>
         </div>
-      </div>
 
-      {/* End Jobs Card */}
-    </div>
+        {/* End Filter */}
+
+        <div>
+          <p className="text-center pb-5 font-bold ">
+            {jobs.length} total jobs found.
+          </p>
+        </div>
+
+        {/* Start Jobs Card */}
+
+        <div className="grid gird-cols-1 gap-4">
+          {jobs?.map((job) => (
+            <PostedJob key={job._id} job={job}></PostedJob>
+          ))}
+        </div>
+
+        {/* End Jobs Card */}
+      </div>
+    </section>
   );
 };
 
--- pages/jobs.js
+++ pages/jobs.js
@@ -3,14 +3,45 @@ import Layout from "../components/layout/Layout";
 import Footer from "../components/Shared/Footer/Footer";
 import Header from "../components/Shared/Header/Header";
 
-const jobs = () => {
+const jobs = ({ jobs }) => {
   return (
     <Layout title="Jobs">
       <Header />
-      <PostedJobs />
+      <PostedJobs jobs={jobs} />
       <Footer />
     </Layout>
   );
 };
 
 export default jobs;
+
+export const getServerSideProps = async (context) => {
+  const { query } = context;
+  const { jobType } = query;
+
+  // without filtering
+  if (jobType) {
+    const response = await fetch(
+      `https://sheltered-journey-99057.herokuapp.com/jobs/?jobType=${jobType}`
+    );
+    const data = await response.json();
+
+    return {
+      props: {
+        jobs: data.data,
+        count: data.count,
+      },
+    };
+  } else {
+    const response = await fetch(
+      "https://sheltered-journey-99057.herokuapp.com/jobs"
+    );
+    const data = await response.json();
+    return {
+      props: {
+        jobs: data.data,
+        count: data.count,
+      },
+    };
+  }
+};
