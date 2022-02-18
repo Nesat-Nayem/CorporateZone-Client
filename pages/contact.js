321f3c6415a16eb66c2ec0cfeabe7ab2224989e7
coderyeasin <yeasin3233@gmail.com>
2022-02-18 19:27:49 +0600

    define

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,6 @@
 import ContactUs from "../components/ContactUs/ContactUs";
+import Layout from "../components/layout/Layout";
+import Footer from "../components/Shared/Footer/Footer";
 import Header from "../components/Shared/Header/Header";
 
 const Contact = () => {
