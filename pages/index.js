bd0eabb9685c0c2dc19221b15336d411afa013d0
Nesat-Nayem <nesatnayem042@gmail.com>
2022-03-04 19:02:39 +0600

    price add

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       {/* <CategoryJobs /> */}
       <RecentJobs />
       <CompanyJobs />
-      {/* <Pricing /> */}
+      <Pricing />
       {/* <Faq /> */}
       {/* <BlogArea /> */}
       <Rule />
