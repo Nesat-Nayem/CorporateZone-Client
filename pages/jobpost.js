552a617c4f4690e3d702f23605897c2ce3d97839
Nesat-Nayem <nesatnayem042@gmail.com>
2022-03-16 23:15:11 +0600

    got error

--- pages/jobpost.js
+++ pages/jobpost.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import JobPost from '../components/Dashboard/PostJob/JobPost';
 
-const Jobpost = () => {
+const PostJobs = () => {
     return (
         <>
             <JobPost/>
@@ -9,4 +9,4 @@ const Jobpost = () => {
     );
 };
 
-export default Jobpost;
+export default PostJobs;
