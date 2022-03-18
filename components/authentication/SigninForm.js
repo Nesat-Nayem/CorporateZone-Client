23f15dee434db834231433a32f1a39ea582773e4
coderyeasin <yeasin3233@gmail.com>
2022-03-18 07:14:01 +0600

    login design

--- components/authentication/SigninForm.js
+++ components/authentication/SigninForm.js
@@ -1,5 +1,8 @@
 import { useForm } from "react-hook-form";
 import useFirebase from "../../redux/slices/user/useFirebase";
+import job from "../../images/job_hunt.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 const SigninForm = () => {
   const { logInWithEmailAndPassword } = useFirebase();
@@ -11,49 +14,88 @@ const SigninForm = () => {
   } = useForm();
   const onSubmit = (data) => {
     logInWithEmailAndPassword(data.email, data.password);
+    console.log(data.email, data.password);
 
     //  router.push("/");
   };
   return (
     <>
-      <form className="md:w-96 my-10" onSubmit={handleSubmit(onSubmit)}>
-        <div className="my-4">
-          <input
-            className="w-full block  border-b-2 border-cyan-500 py-2 rounded  text-black focus:outline-none placeholder:text-black px-2"
-            placeholder="Email"
-            type="email"
-            {...register("email", { required: true })}
-          />
-          {/* errors will return when field validation fails  */}
-          {errors.email && (
-            <span className="text-sm text-red-500 block">
-              email is required
-            </span>
-          )}
-        </div>
-        <div className="my-4">
-          <input
-            className="w-full block  border-b-2 border-cyan-500 py-2 rounded  text-black focus:outline-none placeholder:text-black px-2"
-            placeholder="Password"
-            type="password"
-            {...register("password", {
-              required: true,
-              pattern: /^(?=.{6,})/,
-            })}
-          />
-          <span className="text-sm text-red-500 block">
-            {errors.password?.type === "required" && "password  is required"}
-          </span>
-          <span className="text-sm text-red-500 block">
-            {errors.password?.type === "pattern" &&
-              "password must be 6 characters"}
-          </span>
-        </div>
-        <div className="text-center py-5">
-          <input
-            className="  py-2 border  border-cyan-500 text-cyan-500 px-7 rounded font-medium  focus:outline-none cursor-pointer hover:bg-cyan-500 hover:text-white transition duration-300"
-            type="submit"
-          />
+      <form onSubmit={handleSubmit(onSubmit)} action="#">
+        <div className="flex items-center min-h-screen bg-gray-50">
+          <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
+            <div className="flex flex-col md:flex-row">
+              <div className="h-32 md:h-auto md:w-1/2 md:order-last">
+                <Image
+                  className="object-cover w-full h-full"
+                  src={job}
+                  alt="img"
+                />
+              </div>
+              <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                <div className="w-full">
+                  <div className="flex justify-center">
+                    <img
+                      className="my-3"
+                      src="https://i.ibb.co/V29yZ2z/logo-zone.png"
+                      alt="logo"
+                    />
+                  </div>
+                  <h1 className="mb-4 text-2xl font-bold text-center text-gray-700">
+                    Sign In
+                  </h1>
+                  <div className="mt-4">
+                    <label className="block text-sm">Email</label>
+                    <input
+                      type="email"
+                      className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                      placeholder="Email Address"
+                      {...register("email", { required: true })}
+                    />
+                    {errors.email && (
+                      <span className="text-sm text-red-500 block">
+                        email is required
+                      </span>
+                    )}
+                  </div>
+                  <div>
+                    <label className="block mt-4 text-sm">Password</label>
+                    <input
+                      className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                      placeholder="Password"
+                      type="password"
+                      {...register("password", {
+                        required: true,
+                        pattern: /^(?=.{6,})/,
+                      })}
+                    />
+                    <span className="text-sm text-red-500 block">
+                      {errors.password?.type === "required" &&
+                        "password  is required"}
+                    </span>
+                    <span className="text-sm text-red-500 block">
+                      {errors.password?.type === "pattern" &&
+                        "password must be 6 characters"}
+                    </span>
+                  </div>
+                  <button
+                    className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+                    href="#"
+                  >
+                    Sign in
+                  </button>
+
+                  <div className="mt-4 text-center">
+                    <p className="text-sm">
+                      Don't have an account yet?{" "}
+                      <Link href="/signup">
+                        <a className="text-blue-600 hover:underline">Sign up.</a>
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </form>
     </>
--- pages/signin.js
+++ pages/signin.js
@@ -11,16 +11,7 @@ const signin = () => {
       <Header />
       <section className="bg-gray-100 dark:bg-slate-700">
         <div className="md:w-9/12 w-11/12 mx-auto flex items-center justify-center py-20">
-          <div className="text-center">
-            <h1 className="text-5xl font-bold font-serif dark:text-white">
-              Sign in{" "}
-            </h1>
-            <p className="font-semibold dark:text-slate-200 py-1">
-              {"Don't have an account? "}
-              <Link href="/signup">
-                <a className="text-blue-700 hover:underline"> Sign up</a>
-              </Link>
-            </p>
+          <div className="text-center mt-10">
 
             {/* sign up form */}
             <SigninForm />
