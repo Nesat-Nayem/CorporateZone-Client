a8b4cbd3c91b1d4b239a862bb3bae380b370c137
coderyeasin <yeasin3233@gmail.com>
2022-03-21 18:06:42 +0600

    fix

--- components/Works/Works.js
+++ components/Works/Works.js
@@ -15,39 +15,39 @@ const Works = () => {
 },[])
   return (
     <div className="py-5 relative">
-      <h3 className="text-3xl text-center after:content-[''] after:absolute after:border-t-2 after:w-16 md:after:border-cyan-500 after:left-0 after:bottom-0 after:right-60 after:top-10 after:mx-auto font-medium text-slate-700 uppercase my-20">
+      <h3 className="text-3xl text-center after:content-[''] after:absolute after:border-t-2 after:w-16 md:after:border-cyan-500 after:left-0 after:bottom-0 after:right-60 after:top-10 after:mx-auto font-medium text-slate-700 dark:text-slate-50 uppercase my-20">
         How Does IT Works
       </h3>
 
       <div className="fascinate md:grid grid-cols-2 lg:w-9/12 md:w-10/12 mx-auto ">
         <div
-          className="space-y-7 mx-20"
+          className="space-y-7 mx-20 dark:text-slate-50"
           data-aos="fade-right"
           data-aos-offset="300"
           data-aos-easing="ease-in-sine"
         >
           <h3 className="text-2xl font-medium"> Fascinate</h3>
-          <p className="leading-7 max-w-[500px] text-slate-700">
+          <p className="leading-7 max-w-[500px] text-slate-700 dark:text-slate-50">
             Super user-friend user interface along with customization options to
             make your jobs look attractive for the potential candidates.
           </p>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Showcase Stunning Company Page
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Highlight Work Culture & Benefits
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Easy To Create Candidate Profile
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Interactive Job Application
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Form In-app Messaging With Candidates
           </li>
@@ -78,28 +78,28 @@ const Works = () => {
           data-aos-offset="400"
           data-aos-easing="ease-in-sine"
         >
-          <h3 className="text-2xl font-medium"> Process</h3>
-          <p className="leading-7 max-w-[500px] text-slate-700">
+          <h3 className="text-2xl font-medium dark:text-slate-50"> Process</h3>
+          <p className="leading-7 max-w-[500px] text-slate-700 dark:text-slate-50">
             Personalized team management system along with smart tracking and
             engaging user experience to make the recruitment process faster.
           </p>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700  dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Advanced Applicant Tracking & Filter
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Automatic Resume Parsing & Analyze
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Smart & Quick Evaluation Process
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Smart & Quick Evaluation Process
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Effortless Interview Scheduling
           </li>
@@ -113,28 +113,28 @@ const Works = () => {
           data-aos-offset="400"
           data-aos-easing="ease-in-sine"
         >
-          <h3 className="text-2xl font-medium"> Recruit</h3>
-          <p className="leading-7 max-w-[500px] text-slate-700">
+          <h3 className="text-2xl font-medium dark:text-slate-50"> Recruit</h3>
+          <p className="leading-7 max-w-[500px] text-slate-700 dark:text-slate-50">
             Seamless and advanced recruitment system to make the best hiring
             decisions & create the most efficient team with the right people.
           </p>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Specialized Note For Team
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Advanced Scoring Process
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Customizable Pipeline
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Remote Hiring Ready
           </li>
-          <li className="flex text-slate-700">
+          <li className="flex text-slate-700 dark:text-slate-50">
             <FcOk className="mx-3 text-[#36b9f7]" />
             Automated Offer Letter
           </li>
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,11 +4,9 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { initialUserData } from "../../redux/slices/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import initializeAuthentication from "../../firebase";
-import cover from '../../styles/Home.module.css'
 import { ThemeProvider } from "../Shared/Header/themeContext";
 
-const Layout = ({ title, children }) =>
-{
+const Layout = ({ title, children }) => {
   initializeAuthentication();
 
   const currentUser = useSelector((state) => state.user.currentUser);
@@ -16,10 +14,8 @@ const Layout = ({ title, children }) =>
   const dispatch = useDispatch();
   const auth = getAuth();
   //   // observer function
-  useEffect(() =>
-  {
-    const unsubscribe = onAuthStateChanged(auth, (user) =>
-    {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         dispatch(initialUserData({ user }));
       } else {
