83503d409acf2623036f44c9121b1645ed5f3085
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-03-23 00:18:26 +0600

    notify polish

--- components/Shared/Footer/Footer.js
+++ components/Shared/Footer/Footer.js
@@ -8,36 +8,40 @@ import Swal from "sweetalert2";
 import { useRef } from "react";
 
 
-const Footer = () => {
+const Footer = () =>
+{
 
-   const form = useRef();
+  const form = useRef();
 
-   const sendEmail = (e) => {
-     e.preventDefault();
+  const sendEmail = (e) =>
+  {
+    e.preventDefault();
 
-     emailjs.sendForm(
-         "service_xgz22ma",
-         "template_kmbg1t2",
-         form.current,
-         "iTYZHZvfqHjV6ixNT"
-       )
-       .then(
-         (result) => {
-           console.log(result.text);
-           if (result.text) {
-             return Swal.fire(
-               "Thank You For Subscription!",
-               "You will get skills required jobs within 24hrs!",
-               "success"
-             );
-           } 
-           useRef.current('')
-         },
-         (error) => {
-           console.log(error.text);
-         }
-       );
-   };
+    emailjs.sendForm(
+      "service_xgz22ma",
+      "template_kmbg1t2",
+      form.current,
+      "iTYZHZvfqHjV6ixNT"
+    )
+      .then(
+        (result) =>
+        {
+          console.log(result.text);
+          if (result.text) {
+            return Swal.fire(
+              "Thank You For Subscription!",
+              "You will get skills required jobs within 24hrs!",
+              "success"
+            );
+          }
+          useRef.current('')
+        },
+        (error) =>
+        {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <footer className="">
@@ -62,12 +66,12 @@ const Footer = () => {
           <div className="relative">
             <form action="#" ref={form} onSubmit={sendEmail}>
               <input
-                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
                 placeholder="Enter your mail..."
                 type="text"
                 name="search"
               />
-              <button className="bg-[#42C2FF] hover:bg-white px-3 py-2 text-white hover:text-[#42C2FF] font-medium rounded-lg absolute top-0 right-0">
+              <button className="bg-[#42C2FF] hover:bg-white px-3 py-2 text-white hover:text-[#42C2FF] font-medium rounded-lg absolute top-0 -right-16">
                 Subscribe
               </button>
             </form>
