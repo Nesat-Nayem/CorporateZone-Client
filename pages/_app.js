879701d347907fc9a77689e55e6ca4728751d193
Morsalin <morsalinkausar043@gmail.com>
2022-03-22 21:41:15 +0600

    done preloader

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
+import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
-import Layout from "../components/layout/Layout"
+import Layout from "../components/layout/Layout";
+import Preloader from "../components/preloader/Preloader";
 
 function MyApp({ Component, pageProps }) {
+  const [loader, SetLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => window.addEventListener("load", SetLoader(false)), 1000);
+  }, [loader]);
+
+  if (loader) {
+    return (
+      <>
+        <Preloader />
+      </>
+    );
+  }
   return (
     <Provider store={store}>
       <Layout>
