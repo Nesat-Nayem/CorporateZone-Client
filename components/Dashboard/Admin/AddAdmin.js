fad14b0dbde839eeb72c22eac8c815305dfacf0f
coderyeasin <yeasin3233@gmail.com>
2022-03-23 10:24:30 +0600

    fix issues

--- components/Dashboard/Admin/AddAdmin.js
+++ components/Dashboard/Admin/AddAdmin.js
@@ -1,52 +1,28 @@
 import React from "react";
-import style from "./AddAdmin.module.css";
+import { useForm } from "react-hook-form";
+
+// import ima from "../../../images/career_progress.svg";
 
 const AddAdmin = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = data => { console.log(data) }
+  
   return (
-    <div className={style.add}>
-      <h1>ADD ADMIN</h1>
-      <hr />
-      <div className={style.admin}>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              backgroundColor: "#311838",
-              padding: "40px 20px",
-              borderRadius: "20px",
-            }}
-          >
-            <form>
-              <div className="relative z-0 mb-6 w-full group">
-                <input
-                  type="email"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required=""
-                />
-                <label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                  Email address
-                </label>
-              </div>
-              <button
-                style={{ width: "300px" }}
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
+    <div className="">
+      <form className="w-2/4 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="block w-full bg-transparent outline-none border-b-2 py-2 px-4  placeholder-slate-500 focus:bg-teal-100"
+          type="email"
+          placeholder="Email"
+          {...register("email")}
+        />
 
-        
-
-      </div>
+        <input
+          className=" cursor-pointer block w-full bg-transparent outline-none border-b-2 py-2 px-4 bg-teal-800 text-lg text-gray-50  transform duration-500 hover:scale-95"
+          type="submit"
+          value="Make Admin"
+        />
+      </form>
     </div>
   );
 };
